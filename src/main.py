4ca247c23dee819722cfb72c1be4333c229a170c
Piyush Rungta <piyushrungta25@gmail.com>
2017-10-12 12:47:23 +0530

    Added k-means and spectral clustering and adjusted rand index

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 
 import numpy as np
+from scipy.cluster.vq import whiten , kmeans, vq
+from sklearn.metrics import adjusted_rand_score
+from sklearn import cluster
 
 dataset1_filepath = "data/dataset1_normalised.txt"
 dataset2_filepath = "data/dataset2_normalised.txt"
+
+def randomise_data(X, Y):
+	permutation_array = np.random.permutation(len(X))
+	X = X[permutation_array]
+	Y = Y[permutation_array]
+
+	return X, Y
 
 def get_dataset(filepath):
 	"""Return randomised X, Y for the filepath supplied """
@@ -20,14 +30,36 @@ def get_dataset(filepath):
 	Y = np.asarray(Y, dtype=int)
 
 	# randomise the data
-	permutation_array = np.random.permutation(len(X))
-	X = X[permutation_array]
-	Y = Y[permutation_array]
+	X, Y = randomise_data(X, Y)
 
 	return X, Y
 
+def get_kmeans_clusters(X, Y):
+	# no of clusters = number of unique values in Y
+	k = len(np.unique(Y))
 
+	# whitened = whiten(X)
+	codebook, _ = kmeans(X, k)
+	clusters, _ = vq(X, codebook)
+
+	return clusters
+
+def get_spectral_clusters(X, Y):
+	# no of clusters = number of unique values in Y
+	k = len(np.unique(Y))
+
+	spectral = cluster.SpectralClustering(n_clusters=k, eigen_solver='arpack', affinity="nearest_neighbors")
+	clusters = spectral.fit_predict(X)
+
+	return clusters
+
+def get_adjusted_rand_score(labels_true, label_pred):
+	return adjusted_rand_score(labels_true, label_pred)
 
 
 if __name__ == "__main__":
-	pass
+	for _ in range(10):
+		X, Y = get_dataset(dataset1_filepath)
+		pred = get_spectral_clusters(X, Y)
+		print get_adjusted_rand_score(Y, pred)
+
