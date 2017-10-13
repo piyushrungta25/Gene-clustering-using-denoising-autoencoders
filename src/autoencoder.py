f97bd0f41d7efb7ad0623d555d331f86270308ee
Piyush Rungta <piyushrungta25@gmail.com>
2017-10-13 11:13:23 +0530

    Computing and plotting results

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -4,6 +4,8 @@ import itertools
 import numpy as np
 import main as m
 
+import matplotlib.pyplot as plt
+
 from keras.layers import Input, Dense
 from keras.models import Model
 from keras.optimizers import SGD
@@ -12,7 +14,7 @@ no_of_hidden_nodes = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 error_rate = [0, 0.05, 0.1, 0.15, 0.2]
 learning_rate = 0.1
 no_epochs = 2000
-batch_size = 4
+batch_size = 8
 
 def get_autoenc_model(n):
 	input_img = Input(shape=(17,))
@@ -43,7 +45,7 @@ def train(X, X_noisy, autoencoder, verbosity = 2):
 		epochs=no_epochs,
 		batch_size=batch_size,
 		verbose = verbosity,
-		# validation_data = (X, X)
+		validation_data = (X_noisy, X)
 		)
 
 def train_one(X, Y, n, e):
@@ -55,8 +57,30 @@ def train_one(X, Y, n, e):
 
 	return m.get_score(X_regen, Y)
 
+def plot_result(res, baseline):
+	colors = ['red', 'pink', 'green', 'yellow', 'black']
+
+	plt.plot(no_of_hidden_nodes, [baseline] * len(res[0]), color='blue', label='baseline', linestyle='-')
+
+	for err,color, rate in zip(res, colors, error_rate):
+		plt.plot(no_of_hidden_nodes, err, color=color, label=str(rate*100)+'%', linestyle='-')
+
+	plt.legend(loc = 'upper right')
+	plt.xlabel('No of hidden nodes')
+
+	plt.ylabel('Adjusted Rand Index')
+	plt.savefig('result.svg')
+
 if __name__ == '__main__':
 	X, Y = m.get_dataset(m.dataset1_filepath)
+	baseline = m.get_score(X, Y)
+	# print train_one(X, Y, 16, 0.1)
+	res = np.empty((len(error_rate), len(no_of_hidden_nodes)), dtype=float)
 
-	for n, e in itertools.product(no_of_hidden_nodes, error_rate):
-		print train_one(X, Y, n, e)
+	for i in range(len(error_rate)):
+		for j in range(len(no_of_hidden_nodes)):
+			print "Training for %d hidden nodes %f corruption level" % (no_of_hidden_nodes[j], error_rate[i])
+			ari = train_one(X, Y, no_of_hidden_nodes[j], error_rate[i])
+			res[i][j] = ari
+
+	plot_result(res, baseline)
